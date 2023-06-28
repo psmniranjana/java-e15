52d3d9a33784f040e95f8eb0caf926d6a68585f2
Maleesha Niranjana <117069472+psmniranjana@users.noreply.github.com>
2023-06-28 13:47:30 +0530

    Add files via upload

---
+++ JavaApplication50/src/javaapplication50/JavaApplication50.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication50;
+import java.util.ArrayList;
+import java.util.Scanner;
+/**
+ *
+ * @author Admin
+ */
+public class JavaApplication50 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc=new Scanner(System.in);
+        
+        ArrayList<Integer> evennum = new ArrayList<Integer>();
+        
+       evennum.add(1);
+       evennum.add(2);
+       evennum.add(3);
+       evennum.add(4);
+       evennum.add(5);
+        System.out.println(evennum);
+}
+}
